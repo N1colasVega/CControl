7e83b7f0dbde5be1d9d8bd3ce7ebfa1b7ff9a040
N1colasVega <aspirua@gmail.com>
2016-04-24 15:29:05 +0300

    Рабочий функционал изменений итемов

---
+++ app/src/main/java/com/example/nicolas/ccontrol/changeItemActivity.java
@@ -0,0 +1,74 @@
+package com.example.nicolas.ccontrol;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class changeItemActivity extends AppCompatActivity implements View.OnClickListener {
+    Button Apply, Cancel;
+    EditText nameEd, sumEd, adressEd, descEd;
+
+    Double sumIt; //общая сумма
+    String nameIt,adressIt, descIt;
+
+    int id;
+
+    //Базы данных
+    private DatabaseHelper mDatabaseHelper;
+    private SQLiteDatabase mSqLiteDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_item);
+
+        Apply = (Button) findViewById(R.id.Apply);
+        Cancel = (Button) findViewById(R.id.Cancel);
+
+        Apply.setOnClickListener(this);
+        Cancel.setOnClickListener(this);
+
+        nameEd = (EditText) findViewById(R.id.nameFild);
+        sumEd = (EditText) findViewById(R.id.sumFild);
+        adressEd = (EditText) findViewById(R.id.adressFild);
+        descEd = (EditText) findViewById(R.id.descFild);
+
+        Intent intent = getIntent();
+        nameIt = intent.getStringExtra("nameIt");
+        adressIt = intent.getStringExtra("adressIt");
+        descIt = intent.getStringExtra("descIt");
+        sumIt = intent.getDoubleExtra("sumIt", 2332.0);
+        id = intent.getIntExtra("idTransa",23);
+
+        nameEd.setText(nameIt);
+        adressEd.setText(adressIt);
+        descEd.setText(descIt);
+        sumEd.setText(sumIt.toString());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Apply:
+                mDatabaseHelper = new DatabaseHelper(this, "finalBase2.db", null, 1);//используем простой конструктор(не для даунов,а простой)
+                mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+                ContentValues valuese = new ContentValues();
+                valuese.put(DatabaseHelper.TITLE_COLUM, nameEd.getText().toString());
+                valuese.put(DatabaseHelper.DESC_COLUM, descEd.getText().toString());
+                valuese.put(DatabaseHelper.ADRESS_COLUM, adressEd.getText().toString());
+                valuese.put(DatabaseHelper.SUM_COLUM, Double.parseDouble(sumEd.getText().toString()));
+                mSqLiteDatabase.update(mDatabaseHelper.TABLE_TRANS,valuese,"transaction_id = " + id, null);
+                mSqLiteDatabase.close();
+                finish();
+                break;
+            case R.id.Cancel:
+                finish();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/nicolas/ccontrol/itemActivity.java
+++ app/src/main/java/com/example/nicolas/ccontrol/itemActivity.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class itemActivity extends AppCompatActivity {
 //Активити одного из пунктов списка категории
     //Тут будут фоточки, само описани и так далее
-    TextView dateView,nameView,costView;
+    TextView dateView, nameView, costView, descText, adressText;
 
     //Классы
     controlBD bdcon = new controlBD();
@@ -26,8 +26,8 @@ public class itemActivity extends AppCompatActivity {
     //Переменные
     int id;
     String date; //дата транзикции
-    Double cous; //общая сумма
-    String nameTrans;//Имя транзицкии
+    Double sumIt; //общая сумма
+    String nameIt,adressIt, descIt;
     String format;
 
     @Override
@@ -38,9 +38,17 @@ public class itemActivity extends AppCompatActivity {
         nameView = (TextView) findViewById(R.id.nameView1);
         dateView = (TextView) findViewById(R.id.dateTextV1);
         costView = (TextView) findViewById(R.id.costView);
+        descText = (TextView) findViewById(R.id.descText);
+        adressText = (TextView) findViewById(R.id.adressText);
 
         Intent intent = getIntent();
         id = intent.getIntExtra("transid",23);
+        ReloadItem();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
         ReloadItem();
     }
 
@@ -53,8 +61,16 @@ public class itemActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
         switch (item.getItemId()){
             case R.id.changeItem:
+                intent = new Intent(this,changeItemActivity.class);
+                intent.putExtra("nameIt",nameIt);
+                intent.putExtra("sumIt",sumIt);
+                intent.putExtra("adressIt",adressIt);
+                intent.putExtra("descIt",descIt);
+                intent.putExtra("idTransa",id);
+                startActivityForResult(intent,1);
                 break;
             case R.id.deleteItem:
                 mDatabaseHelper = new DatabaseHelper(this, "finalBase2.db", null, 1);//используем простой конструктор(не для даунов,а простой)
@@ -77,13 +93,21 @@ public class itemActivity extends AppCompatActivity {
             int transName = cursor.getColumnIndex(DatabaseHelper.TITLE_COLUM);
             int transDate = cursor.getColumnIndex(DatabaseHelper.DATE_COLUM);
             int transSum = cursor.getColumnIndex(DatabaseHelper.SUM_COLUM);
+            int transDesc = cursor.getColumnIndex(DatabaseHelper.DESC_COLUM);
+            int transAdress = cursor.getColumnIndex(DatabaseHelper.ADRESS_COLUM);
             do{
                 format = "yyyy.MM.dd.hh:mm";
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
                 String dataTime = simpleDateFormat.format(cursor.getFloat(transDate));
+                nameIt = cursor.getString(transName);
+                adressIt = cursor.getString(transAdress);
+                descIt =  cursor.getString(transDesc);
+                sumIt = cursor.getDouble(transSum);
                 nameView.setText(cursor.getString(transName));
-                dateView.setText("Дата добавления : " + dataTime);
+                dateView.setText("Дата добавления: " + dataTime);
                 costView.setText("Стоимость - " + cursor.getDouble(transSum));
+                descText.setText(cursor.getString(transDesc));
+                adressText.setText("Адресс: " + adressIt);
             }while (cursor.moveToNext());
         }else Log.d("mLog", "0 rows");
         mSqLiteDatabase.close();
